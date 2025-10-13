a92e35abac5a244c30d35230f5ffcb7bdb6605b0
Arthur-Fialho <arthursouza.fp@gmail.com>
2025-10-13 17:52:18 -0300

    chore: Configure DB migrations and finalize Stock microservice MVP

--- src/Estoque.Infrastructure/EstoqueDbContext.cs
+++ src/Estoque.Infrastructure/EstoqueDbContext.cs
@@ -11,5 +11,19 @@ namespace Estoque.Infrastructure
         }
 
         public DbSet<Produto> Produtos { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configuração para a entidade Produto
+            modelBuilder.Entity<Produto>(entity =>
+            {
+                // Define que a propriedade 'Preco' deve ser mapeada para uma coluna
+                // do tipo decimal(18, 2) no banco de dados.
+                // 18 é a precisão total e 2 é o número de casas decimais.
+                entity.Property(p => p.Preco).HasColumnType("decimal(18,2)");
+            });
+        }
     }
 }
